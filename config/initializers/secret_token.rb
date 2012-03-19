82b55525ba3eecaf55f7fdf1c69f2747da0a6532
Aidan Corbett <aidancorbett@Aidans-iMac.local>
2012-03-19 16:01:55 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyRoundtable::Application.config.secret_token = 'b503de821804e8d1df0ab12f5fc2b3e564d33e613ef41af3d3f84afa461574a0697b7c9c4ae21aab70c85765e0451351bcc11c9323bd83eea12a86b4629ec151'
