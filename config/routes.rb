c02ea49c0a375dc9f535e979584a430b6003a0bb
Aidan Corbett <aidancorbett@Aidans-iMac.local>
2012-03-20 17:15:31 +0000

    Login sorted

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 MyRoundtable::Application.routes.draw do
-  get "pages/home"
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
   
-  get "pages/contact"
-  get "pages/about"
+  resources :users
+  resources :sessions
+ 
+  
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -52,7 +57,7 @@ MyRoundtable::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
