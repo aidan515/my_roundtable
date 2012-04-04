6a5f8fced9c32879d6365f74c104abf5aac39a27
Aidan Corbett <aidan515@gmail.com>
2012-04-04 16:24:50 +0100

    All tests passing for Password Reset

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -2,16 +2,14 @@ require "spec_helper"
 
 describe UserMailer do
   describe "password_reset" do
-    let(:mail) { UserMailer.password_reset }
+    let(:user) { Factory(:user, :password_reset_token => "anything")}
+    let(:mail) { UserMailer.password_reset(user) }
 
-    it "renders the headers" do
-      mail.subject.should eq("Password reset")
-      mail.to.should eq(["to@example.org"])
+    it "send user password reset url" do
+      mail.subject.should eq("Password Reset")
+      mail.to.should eq([user.email])
       mail.from.should eq(["from@example.com"])
-    end
-
-    it "renders the body" do
-      mail.body.encoded.should match("Hi")
+      mail.body.encoded.should match(edit_password_reset_path(user.password_reset_token))
     end
   end
 
